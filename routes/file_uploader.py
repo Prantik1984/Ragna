6a966956d79b6e86c7bcd4d69a90c89408ff1831
Prantik <prantik.sarmah84@gmail.com>
2025-10-16 17:41:25 +0530

    updating

--- routes/file_uploader.py
+++ routes/file_uploader.py
@@ -1,16 +1,22 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request,File, UploadFile
 from dotenv import load_dotenv
 import os
 router = APIRouter()
 
 @router.post("/upload/pdfs")
 
-def uploadPdf():
+async def uploadPdf(request: Request, file: UploadFile = File(...)):
     load_dotenv()
-    max_upload_size = os.getenv("Max_Upload_size")* 1024 * 1024
-    return {"status": db_url}
+    max_upload_size = int(os.getenv("Max_Upload_size"))* 1024 * 1024
+    await size_limit(request, max_upload_size)
+    upload_dir=os.getenv("UploadDirectory")
+    os.makedirs(upload_dir, exist_ok=True)
+    file_path = os.path.join(upload_dir, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    return {"status": "File Uploaded Successfully"}
 
-async def size_limit(request: Request, max_bytes: int = 20 * 1024 * 1024):
+async def size_limit(request: Request, max_bytes: int):
     cl = request.headers.get("content-length")
     if cl and int(cl) > max_bytes:
         raise HTTPException(status_code=413, detail="Upload too large.")
